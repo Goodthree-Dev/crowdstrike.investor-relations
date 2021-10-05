b60266e7d276b48e779fa3eb9fa58402cc136db7
Rob Huska <rob.huska85@gmail.com>
2021-10-05 13:32:43 -0400

    WIP header

---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<div class="sticky">
+  <div class="alert-bar">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <p>Learn about the latest cybersecurity news and trends on the CrowdStrike blog.</p>
+        </div>
+        <div class="col-md-6">
+          <a href="#">Explore Blog</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <header class="site-header">
+    <div class="container">
+      <div class="logo"></div>
+
+      <nav>
+        <ul>
+          <li><a href="#">Releases</a></li>
+          <li><a href="#">Events &amp; Presentations</a></li>
+          <li><a href="#">Stock</a></li>
+          <li><a href="#">Financial</a></li>
+          <li><a href="#">Governance</a></li>
+          <li><a href="#">Resources</a></li>
+        </ul>
+        
+        <div class="header-search"></div>
+
+        <a href="#" class="btn black">CrowdStrike.com</a>
+      </nav>
+    </div>
+  </header>
+</div>  
